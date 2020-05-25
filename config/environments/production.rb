6ce3c9e542ed1fabacf57c02c3a9835adce2445c
shreyasamanta <shreysaman@gmail.com>
2020-05-26 00:18:06 +1000

    update project name in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'shrouded-bayou-63405.herokuapp.com'
+  host = 'rails-shreya.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
