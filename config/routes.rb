fec1d601c0118834a9c617f881a496d4ab052034
shreyasamanta <shreysaman@gmail.com>
2020-06-12 11:29:04 +1000

    Up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  get    'sessions/new'
   root   'static_pages#home'
-  get    '/help',   to: 'static_pages#help'
-  get    '/about',  to: 'static_pages#about'
   get    '/contact',to: 'static_pages#contact'
-  get    '/signup', to: 'users#new'
-  get    '/login',  to: 'sessions#new'
-  post   '/login',  to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  post   '/signup', to: 'users#create'
-  resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
