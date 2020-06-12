0df51b03f5eb9c4ae145416c35792afcece11a8b
shreyasamanta <shreysaman@gmail.com>
2020-06-12 16:34:44 +1000

    pass level completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 City.create!(name: "Melbourne",
-    value:"AUS Eastern Standard Time",
-    abbr:"AEST",
     offset:10,
-    isdst: false,
-    text: "(UTC+10:00) Canberra, Melbourne, Sydney",
-    time_zone:"Melbourne"
+    time_zone:"Melbourne",
     )
 
 City.create!(name: "San Francisco",
-    value:"Pacific Daylight Time",
-    abbr:"PDT",
     offset:-7,
-    isdst: true,
-    text: "(UTC-07:00) Pacific Time (US & Canada)",
-    time_zone:"Mountain Time (US & Canada)"
+    time_zone:"Mountain Time (US & Canada)",
     )
 
 City.create!(name: "Tokyo",
-    value:"Japan Standard Time",
-    abbr:"JST",
     offset:9,
-    isdst: false,
-    text: "(UTC+10:00) Osaka, Sapporo, Tokyo",
-    time_zone:"Tokyo"
+    time_zone:"Tokyo",
     )
 
-City.create!(name: "Perth",
-    value:"W. Australia Standard Time",
-    abbr:"WAST",
-    offset:8,
-    isdst: false,
-    text: "(UTC+08:00) Perth",
-    time_zone:"Perth"
+City.create!(name: "Berlin",
+    offset:2,
+    time_zone:"Berlin",
     )
